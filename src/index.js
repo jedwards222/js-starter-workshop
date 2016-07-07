84571d34a8f4d560d41230fe459a24d312708a82
James <jedwards@usagb.net>
2016-07-07 12:07:21 -0400

    first commit of changes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import './style.scss';
+
+var number = 0;
+setInterval(function () {
+  $('#main').html('You\'ve been on this webpage for ' + number + ' seconds!');
+  number++;
+}, 1000);
